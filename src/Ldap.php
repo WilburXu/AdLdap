7ffe22258ae1747b1bc48d3bdf55419b8a035a79
xusongtao <songtao.xu@zhenai.com>
2018-05-29 18:03:58 +0800

    connect

---
+++ src/Ldap.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * User: WilburXu
+ * Date: 2018/5/29
+ * Time: 17:21
+ */
+class Ldap
+{
+    protected $_conn = null;
+
+    public function connect($host = null, $port = 389)
+    {
+        return $this->_conn = ldap_connect($host, $port);
+    }
+
+    /**
+     * __destruct
+     */
+    public function __destruct()
+    {
+        if ($this->_conn) {
+            ldap_close($this->_conn);
+        }
+    }
+}
